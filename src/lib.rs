4f84e59e776ff0a405c4b4d6bf425d6acef98d14
Tom Pridham <pridham.tom@gmail.com>
2021-06-25 15:41:55 -0600

    feat: basic game running now

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,33 @@
+mod mine;
+
 use bevy::prelude::*;
+use mine::Mine;
 use wasm_bindgen::prelude::*;
+
+fn hello_world() {
+    println!("hello world!");
+}
+struct Board;
+//struct Board {
+//grid: Vec<Vec<Mine>>,
+//}
+
+fn generate_board(mut commands: Commands) {
+    commands.spawn().insert(Board);
+}
+struct BoardPlugin;
+
+impl Plugin for BoardPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(generate_board.system());
+    }
+}
 
 #[wasm_bindgen]
 pub fn run() {
     let mut app = App::build();
+    app.add_plugin(BoardPlugin);
+    app.add_system(hello_world.system());
 
     app.add_plugins(DefaultPlugins);
 
